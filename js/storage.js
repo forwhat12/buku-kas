ebf7c22d8549fde9df4d35287ba41a16a1ee6a92
forwhat12 <onongsuherman16@gmail.com>
2026-07-08 20:13:39 +0700

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,10 @@
+// js/storage.js
+export let dataTransaksi = JSON.parse(localStorage.getItem('dataAkuntansiTani')) || [];
+
+export const saveToStorage = (data) => {
+    dataTransaksi = data;
+    localStorage.setItem('dataAkuntansiTani', JSON.stringify(dataTransaksi));
+};
+
+export const getData = () => dataTransaksi;
 
