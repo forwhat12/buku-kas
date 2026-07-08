409e8441dbb6729e1a3d97524c571203f990cea9
forwhat12 <onongsuherman16@gmail.com>
2026-07-08 20:16:29 +0700

    Update transaction.js

--- js/transaction.js
+++ js/transaction.js
@@ -1,1 +1,23 @@
+// js/transaction.js
+import { dataTransaksi, saveToStorage } from './storage.js';
+
+export const tambahTransaksi = (item) => {
+    let currentData = dataTransaksi;
+    currentData.push(item);
+    saveToStorage(currentData);
+};
+
+export const hapusTransaksi = (id) => {
+    let currentData = dataTransaksi.filter(t => t.id !== id);
+    saveToStorage(currentData);
+};
+
+export const updateTransaksi = (id, item) => {
+    let currentData = dataTransaksi;
+    const index = currentData.findIndex(t => t.id === id);
+    if (index !== -1) {
+        currentData[index] = item;
+        saveToStorage(currentData);
+    }
+};
 
